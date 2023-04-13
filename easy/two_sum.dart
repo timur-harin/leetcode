281990457e672a24c0a4b05a9fcef73ff57e3967
Timur Kharin <timurx404@gmail.com>
2023-04-13 12:12:31 +0300

    init

---
+++ easy/two_sum.dart
@@ -0,0 +1,14 @@
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+      var result;
+      for (var i = 0;i<nums.length;i++){
+          if (nums.contains(target-nums[i])){
+              result = [i,nums.indexOf(target-nums[i])];
+              if (result[0]!=result[1]){
+                break;
+            }
+          }
+      }
+      return result;
+  }
+}
